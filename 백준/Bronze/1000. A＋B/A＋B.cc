7dcddb582c69d1ef106bd7ecb187aa03c8cecb94
NaHeun <81688267+zrhmg0b71@users.noreply.github.com>
2023-07-20 20:26:18 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define INF 987654321
+typedef long long ll;
+
+using namespace std;
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+
+  int a, b;
+  cin >> a >> b;
+
+  cout << a + b << endl;
+
+  return 0;
+}
